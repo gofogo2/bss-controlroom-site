8b446c75a1cf7f4a91dce0e4f314c41c5f9a046c
gofogo <promoted.go@gmail.com>
2024-11-12 21:52:14 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,25 +21,25 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       {/* Fixed Resolution Display */}
-      {/* <div className="fixed top-0 left-0 w-full bg-black text-white p-2 text-center z-50">
+      {/* <div className="fixed top-0 left-0 z-50 w-full p-2 text-center text-white bg-black">
         현재 해상도: {windowSize.width} x {windowSize.height}
       </div> */}
- 
+    
       {/* Main Content Container */}
       <div className="flex min-h-screen pt-0"> {/* pt-12 to account for fixed header */}
         {/* Left Image */}
-        <div className="w-1/4 bg-gray-200 p-0">
-          <div className="h-full w-full">
+        <div className="w-1/4 p-0 bg-gray-200">
+          <div className="w-full h-full">
             <img 
               src="/logo512.png"
               alt="Left decoration"
-              className="w-full h-full object-cover"
+              className="object-cover w-full h-full"
             />
           </div>
         </div>
 
         {/* Center Content */}
-        <div className="w-2/4 bg-white p-0">
+        <div className="w-2/4 p-0 bg-white">
           <iframe
             src="https://daamdaam.co.kr"
             className="w-full h-full min-h-[800px]"
@@ -48,12 +48,12 @@ function App() {
         </div>
 
         {/* Right Image */}
-        <div className="w-1/4 bg-gray-200 p-0">
-          <div className="h-full w-full">
+        <div className="w-1/4 p-0 bg-gray-200">
+          <div className="w-full h-full">
             <img 
               src="/logo512.png"
               alt="Right decoration"
-              className="w-full h-full object-cover"
+              className="object-cover w-full h-full"
             />
           </div>
         </div>
